8c88452b42ae3e864f7c67decfdcf0026f266215
AliHMohammad <alihmohammad@hotmail.com>
2024-03-10 12:07:06 +0100

    saved token as enviorenment variable

--- src/HolidayCalendar/ApiService.cs
+++ src/HolidayCalendar/ApiService.cs
@@ -1,15 +1,21 @@
 using System.Net.Http.Headers;
 using System.Text.Json;
 
+
 namespace HolidayCalendar {
 
     public class ApiService {
 
         private readonly string API_URL = "https://api.sallinggroup.com/v1/holidays";
-        private readonly string BEARER_TOKEN = "6829b007-0619-4f05-8949-eaf8842b8ffb";
+        private readonly string BEARER_TOKEN;
         private HttpClient client;
 
         public ApiService() {
+            //Token saved as environment variable
+            BEARER_TOKEN = Environment.GetEnvironmentVariable("BEARER_TOKEN");
+            if (string.IsNullOrEmpty(BEARER_TOKEN))
+                throw new InvalidOperationException("BEARER_TOKEN environment variable is not set");
+            
             client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", BEARER_TOKEN);
         }
@@ -21,7 +27,7 @@ namespace HolidayCalendar {
         
         public List<DateTime> GetHolidays(string startDate, string endDate) {            
             var responseBody = SendGetRequest($"{API_URL}/?startDate={startDate}&endDate={endDate}");
-            var holidays = JsonSerializer.Deserialize<List<Holiday>>(responseBody);
+            var holidays = JsonSerializer.Deserialize<List<HolidayDTO>>(responseBody);
             var dates = holidays?
                 .Where(holiday => holiday.nationalHoliday)
                 .Select(holiday =>
--- src/HolidayCalendar/Holiday.cs
+++ src/HolidayCalendar/Holiday.cs
@@ -2,12 +2,12 @@
 
 namespace HolidayCalendar {
 
-    public class Holiday {
+    public class HolidayDTO {
         public string date { get; set; }
         public string name { get; set; }
         public bool nationalHoliday { get; set; }
 
-        public Holiday(string date, string name, bool nationalHoliday) {
+        public HolidayDTO(string date, string name, bool nationalHoliday) {
             this.date = date;
             this.name = name;
             this.nationalHoliday = nationalHoliday;
